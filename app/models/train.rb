badee68ec564a2ec917e40e53fa64d3757781911
DenisRebenok <denis.rebenok.pro@gmail.com>
2016-02-14 20:45:54 +0200

    Add wagons data to trains views

--- app/models/train.rb
+++ app/models/train.rb
@@ -6,4 +6,16 @@ class Train < ActiveRecord::Base
   # has_many :users, through: :tickets
 
   validates :number, presence: true#, format: {with: /^[а-яa-z\d]{3}-?[а-яa-z\d]{2}$/i, uniqueness: true}
+
+  def wagons_by_type(type_id)
+    wagons.where(wagon_type: type_id)
+  end
+
+  def upper_places_count(type_id)
+    wagons.where(wagon_type: type_id).sum(:upper_places)
+  end
+
+  def lower_places_count(type_id)
+    wagons.where(wagon_type: type_id).sum(:lower_places)
+  end
 end
